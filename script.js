7d11c46d581ed6e7e141ef5cb6f90190406b9d21
jimmykiedis <jinkiedis@icloud.com>
2025-07-17 13:45:03 -0400

    Version 0.4.3

--- script.js
+++ script.js
@@ -15,16 +15,20 @@ audio.addEventListener("ended", () => {
     playPauseBtn.textContent = "▶️";
 });
 
-let imagens = [];
-let indice = 0;
+// Depois que você carregar a lista (ex: do JSON):
+let imagens = [];    // Sua lista com nomes corretos, cria uma lista de imagens
+let indice = 0;         //inicializa a o indicie 0 na lista de imagens
 
-fetch("contents/assets/lista.json")
-  .then(res => res.json())
-  .then(dados => {
+// Na inicialização
+document.getElementById("imagemCarrossel").src = imagens[indice];
+
+fetch("contents/assets/lista.json")     // abre a lista em json criada pelo script python
+  .then(res => res.json())              // aqui a gente converte o json em um objeto javascript
+  .then(dados => {                      // atribui o objeto javascript a variável da lista de imagens
     imagens = dados;
     atualizarImagem(); // mostra a primeira imagem assim que carregar
   })
-  .catch(err => {
+  .catch(err => {                       // se der erro, avisa no console
     console.error("Erro ao carregar lista de imagens:", err);
   });
 
